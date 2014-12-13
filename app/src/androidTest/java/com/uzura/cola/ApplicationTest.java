83c7cbb50935da49c195dacdfd7446abdfab30c7
uzura <uzura_contact@yahoo.co.jp>
2014-12-13 20:37:12 +0900

    最初のコミット。

---
+++ app/src/androidTest/java/com/uzura/cola/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.uzura.cola;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
