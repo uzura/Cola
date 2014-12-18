80366562c913eea84e990a24ff9b945e6047885d
uzura <uzura_contact@yahoo.co.jp>
2014-12-18 22:46:56 +0900

    crittersism導入(最低限)

--- app/src/main/java/com/uzura/cola/MainActivity.java
+++ app/src/main/java/com/uzura/cola/MainActivity.java
@@ -1,5 +1,6 @@
 package com.uzura.cola;
 
+import com.crittercism.app.Crittercism;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,10 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Crittercism
+        Crittercism.initialize(getApplicationContext(), "CRITTERCISM_APP_ID"); //Crittercism App ID
+
         setContentView(R.layout.activity_main);
     }
 
